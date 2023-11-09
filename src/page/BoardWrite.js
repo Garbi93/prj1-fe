bd0684892678b81d87bc9ce972f3f547f7a6108c
Garbi93 <iseung2093@gmail.com>
2023-11-09 14:30:30 +0900

    잘 저장이 된 후 home으로 이동

--- src/page/BoardWrite.js
+++ src/page/BoardWrite.js
@@ -9,12 +9,15 @@ import {
   useToast,
 } from "@chakra-ui/react";
 import axios from "axios";
+import { useNavigate } from "react-router-dom";
 
 export function BoardWrite() {
   const [title, setTitle] = useState("");
   const [content, setContent] = useState("");
   const [writer, setWriter] = useState("");
   const [isSubmitting, setIsSubmitting] = useState(false);
+
+  const navigate = useNavigate();
 
   let toast = useToast();
 
@@ -28,6 +31,7 @@ export function BoardWrite() {
           description: "새 글이 저장 되었습니다.",
           status: "success",
         });
+        navigate("/");
       })
       .catch((error) => {
         console.log(error.response.status);
