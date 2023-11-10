832a76498b592fbe598633aae1e339dd1bb16b11
Garbi93 <iseung2093@gmail.com>
2023-11-10 09:55:01 +0900

    11/10 git test

--- src/page/BoardEdit.js
+++ src/page/BoardEdit.js
@@ -62,6 +62,7 @@ export function BoardEdit() {
               draft.writer = e.target.value;
             });
           }}
+        {/*  내용 수정*/}
         />
       </FormControl>
       <Button colorScheme="blue">저장</Button>
