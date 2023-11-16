dfde6c4065e7dabf8a41fe3454d5551f9851a434
Garbi93 <iseung2093@gmail.com>
2023-11-16 16:31:34 +0900

    11/16 좋아요 기능 추가

--- src/page/member/MemberLogin.js
+++ src/page/member/MemberLogin.js
@@ -28,6 +28,7 @@ export function MemberLogin() {
           description: "로그인 되었습니다.",
           status: "info",
         });
+        navigate("/");
       })
       .catch(() => {
         toast({
