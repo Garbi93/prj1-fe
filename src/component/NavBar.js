261acfabc5f8d7c2c36589afeb6d01e1f09e02e4
Garbi93 <iseung2093@gmail.com>
2023-11-22 09:22:14 +0900

    11/22 네브바 버튼 정렬

--- src/component/NavBar.js
+++ src/component/NavBar.js
@@ -1,4 +1,4 @@
-import { Button, Flex, useToast } from "@chakra-ui/react";
+import { Button, Flex, Spacer, useToast } from "@chakra-ui/react";
 import { useLocation, useNavigate } from "react-router-dom";
 import axios from "axios";
 import { useContext, useEffect } from "react";
@@ -66,6 +66,7 @@ export function NavBar() {
           작성
         </Button>
       )}
+      <Spacer />
       {isAuthenticated() || (
         <Button
           borderRadius={0}
